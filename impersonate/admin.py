0158337ffa25f4a964b04f63fb2e3407eabfd324
Hugo Rodger-Brown <hugo@yunojuno.com>
2015-11-01 18:30:22 +0000

    Add session state filter to ImpersonationLog admin list
    Allows filtering based on whether the session was closed out or not -
    marked by the presence of a session_ended_at value - which in turn
    can be used to filter for open session - i.e. people impersonating
    at the current time.
    
    --HG--
    branch : impersonation-log

--- impersonate/admin.py
+++ impersonate/admin.py
@@ -13,6 +13,29 @@ def friendly_name(user):
         return user.username
 
 
+class SessionStateFilter(admin.SimpleListFilter):
+
+    """Custom admin filter based on the session state."""
+
+    title = 'session state'
+    parameter_name = 'session'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('incomplete', "Incomplete"),
+            ('complete', "Complete")
+        )
+
+    def queryset(self, request, queryset):
+        print queryset
+        if self.value() == 'incomplete':
+            return queryset.filter(session_ended_at__isnull=True)
+        if self.value() == 'complete':
+            return queryset.filter(session_ended_at__isnull=False)
+        else:
+            return queryset
+
+
 class ImpersonationLogAdmin(admin.ModelAdmin):
     list_display = (
         'impersonator_',
@@ -29,7 +52,7 @@ class ImpersonationLogAdmin(admin.ModelAdmin):
         'session_ended_at',
         'duration'
     )
-    list_filter = ('session_started_at',)
+    list_filter = (SessionStateFilter, 'session_started_at')
 
     def impersonator_(self, obj):
         return friendly_name(obj.impersonator)
