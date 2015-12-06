72c11ea0a41ba46cd017a156a1980b0de2b851c2
Federico Bond <federicobond@gmail.com>
2015-12-05 22:18:27 -0300

    Replace deprecated string view arguments to url

--- impersonate/urls.py
+++ impersonate/urls.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+
+from .views import stop_impersonate, list_users, search_users, impersonate
 
 
-urlpatterns = patterns('impersonate.views',
+urlpatterns = [
     url(r'^stop/$',
-        'stop_impersonate',
+        stop_impersonate,
         name='impersonate-stop'),
     url(r'^list/$',
-        'list_users',
+        list_users,
         {'template': 'impersonate/list_users.html'},
         name='impersonate-list'),
     url(r'^search/$',
-        'search_users',
+        search_users,
         {'template': 'impersonate/search_users.html'},
         name='impersonate-search'),
     url(r'^(?P<uid>.+)/$',
-        'impersonate',
+        impersonate,
         name='impersonate-start'),
-)
+]
