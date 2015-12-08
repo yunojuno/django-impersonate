aa4981e0a97f613107dfb7729534a432bae7b73d
Erik Telepovský <erik.telepovsky@email.com>
2015-12-08 11:52:02 +0100

    urlpatterns is list of urls instead of using patterns method

--- impersonate/urls.py
+++ impersonate/urls.py
@@ -4,19 +4,23 @@ from django.conf.urls import patterns, url
 from impersonate.views import impersonate, search_users, list_users, stop_impersonate
 
 
-urlpatterns = patterns('impersonate.views',
+urlpatterns = [
     url(r'^stop/$',
         stop_impersonate,
-        name='impersonate-stop'),
+        name='impersonate-stop',
+        prefix='impersonate.views'),
     url(r'^list/$',
         list_users,
         {'template': 'impersonate/list_users.html'},
-        name='impersonate-list'),
+        name='impersonate-list',
+        prefix='impersonate.views'),
     url(r'^search/$',
         search_users,
         {'template': 'impersonate/search_users.html'},
-        name='impersonate-search'),
+        name='impersonate-search',
+        prefix='impersonate.views'),
     url(r'^(?P<uid>.+)/$',
         impersonate,
-        name='impersonate-start'),
-)
+        name='impersonate-start',
+        prefix='impersonate.views'),
+]
