078725844019022664b89508343078c9b0deba18
Erik Telepovský <erik.telepovsky@email.com>
2015-12-08 11:34:12 +0100

    RemovedInDjango110Warning fix

--- impersonate/urls.py
+++ impersonate/urls.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 
+from impersonate.views import impersonate, search_users, list_users, stop_impersonate
+
 
 urlpatterns = patterns('impersonate.views',
     url(r'^stop/$',
-        'stop_impersonate',
+        stop_impersonate,
         name='impersonate-stop'),
     url(r'^list/$',
-        'list_users',
+        list_users,
         {'template': 'impersonate/list_users.html'},
         name='impersonate-list'),
     url(r'^search/$',
-        'search_users',
+        search_users,
         {'template': 'impersonate/search_users.html'},
         name='impersonate-search'),
     url(r'^(?P<uid>.+)/$',
-        'impersonate',
+        impersonate,
         name='impersonate-start'),
 )
