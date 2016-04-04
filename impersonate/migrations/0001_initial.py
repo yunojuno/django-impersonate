bf9b2151488a36e8c2c062f0c66a8743220602c1
Yuri Prezument <y@yprez.com>
2016-04-05 01:18:40 +0300

    Fix unicode strings in migration
    Fixes #31
    
    --HG--
    branch : fix-migration

--- impersonate/migrations/0001_initial.py
+++ impersonate/migrations/0001_initial.py
@@ -16,11 +16,11 @@ class Migration(migrations.Migration):
             name='ImpersonationLog',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('session_key', models.CharField(help_text=b'The Django session request key.', max_length=40)),
-                ('session_started_at', models.DateTimeField(help_text=b'The time impersonation began.', null=True, blank=True)),
-                ('session_ended_at', models.DateTimeField(help_text=b'The time impersonation ended.', null=True, blank=True)),
-                ('impersonating', models.ForeignKey(related_name='impersonated_by', to=settings.AUTH_USER_MODEL, help_text=b'The user being impersonated.')),
-                ('impersonator', models.ForeignKey(related_name='impersonations', to=settings.AUTH_USER_MODEL, help_text=b'The user doing the impersonating.')),
+                ('session_key', models.CharField(help_text='The Django session request key.', max_length=40)),
+                ('session_started_at', models.DateTimeField(help_text='The time impersonation began.', null=True, blank=True)),
+                ('session_ended_at', models.DateTimeField(help_text='The time impersonation ended.', null=True, blank=True)),
+                ('impersonating', models.ForeignKey(related_name='impersonated_by', to=settings.AUTH_USER_MODEL, help_text='The user being impersonated.')),
+                ('impersonator', models.ForeignKey(related_name='impersonations', to=settings.AUTH_USER_MODEL, help_text='The user doing the impersonating.')),
             ],
         ),
     ]
