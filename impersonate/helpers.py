022a0a38774f90234d0b89f79e2de33bf9bc4872
j <j@mailb.org>
2017-07-21 12:31:55 +0000

    Pagination may yield inconsistent results with an unordered object_list, order users by username
    --HG--
    branch : r-w-x/pagination-may-yield-inconsistent-result-1500640313140

--- impersonate/helpers.py
+++ impersonate/helpers.py
@@ -90,7 +90,7 @@ def users_impersonable(request):
         )
         return custom_queryset_func(request)
     else:
-        return User.objects.all()
+        return User.objects.all().order_by('username')
 
 
 def check_allow_for_user(request, end_user):
