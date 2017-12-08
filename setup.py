cae6a8129a35666e073d3db0e45e81da31cad1d8
Peter Sanchez <peter@netlandish.com>
2017-12-07 16:33:49 -0800

    Updating setup metadata

--- setup.py
+++ setup.py
@@ -45,10 +45,11 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Environment :: Web Environment',
     ],
 )
