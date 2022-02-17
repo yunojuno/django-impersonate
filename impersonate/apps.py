2d63996052ee8babb867c1011a22bfc9faaadf3d
Peter Sanchez <pjs@petersanchez.com>
2022-02-17 11:09:57 -0600

    Adding 'default_auto_field' definition. Closes ~petersanchez/django-impersonate#62

--- impersonate/apps.py
+++ impersonate/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'impersonate'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         from . import signals
